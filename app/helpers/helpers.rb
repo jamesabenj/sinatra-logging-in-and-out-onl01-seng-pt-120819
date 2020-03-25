361eacbf42f5c432be16c0c3f0be1f4be4ede605
James Benjamin <james.abenj@gmail.com>
2020-03-25 18:53:51 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,13 @@
 class Helpers
 
-  def current_user(session)
+  def self.current_user(session)
     @current_user ||= User.find(session[:user_id]) if
     session[:user_id].present?
     @current_user
     binding.pry
   end
 
-  def is_logged_in?
+  def self.is_logged_in?
     !!current_user
   end
 end
