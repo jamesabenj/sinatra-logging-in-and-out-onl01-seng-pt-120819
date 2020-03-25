4c454f2da6a2538a4d1c8567a89e58f7e13c2c56
James Benjamin <james.abenj@gmail.com>
2020-03-25 18:52:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find(session[:user_id])
+    binding.pry
     if Helpers.is_logged_in?
       erb :account
     else
